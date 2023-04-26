cb88dd59d48a75f9ce18c6ef3222622fe72cdf05
Boudhayan Biswas <hello@boudhayan.com>
2023-04-27 01:40:49 +0530

    change blog name

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Swift Nonsense",
+    description: "Let's talk nonsense about Swift",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
